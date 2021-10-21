0e779e4f9b814f67d6350aedacf4d55ace18fb24
DongShanHu <ken83924@gmail.com>
2021-10-21 22:03:12 +0800

    10/24 base for adding mongodDB

---
+++ src/main/java/com/example/demo/api/ProductController.java
@@ -0,0 +1,61 @@
+package com.example.demo.api;
+
+
+import com.example.demo.model.Product;
+import com.example.demo.parameter.ProductQueryParameter;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") String id) {
+        Product product = productService.getProduct(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts(@ModelAttribute ProductQueryParameter param) {
+        List<Product> products = productService.getProducts(param);
+        return ResponseEntity.ok(products);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product request) {
+        Product product = productService.createProduct(request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(product.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> replaceProduct(
+            @PathVariable("id") String id, @RequestBody Product request) {
+        Product product = productService.replaceProduct(id, request);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable("id") String id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
